f316f26fd85af3c62d321d733a2341d4ba65d179
alexanderlewisstevens <80877580+alexanderlewisstevens@users.noreply.github.com>
2025-06-09 23:36:57 +0000

    Refactor LaTeX generation scripts to improve configurability and structure; add margin and header options, and update problem inputs in main and solution files.

--- generate_main.py
+++ generate_main.py
@@ -1,19 +1,26 @@
 import os
 import random
 
-BANK_DIR = "Bank1"
-PROBLEM_PREFIX = "problem"
-PROBLEM_SUFFIX = ".tex"
+# === CONFIGURABLE CONSTANTS ===
+BANK_DIR = "Bank1"  # Directory containing problem .tex files
+PROBLEM_PREFIX = "problem"  # Prefix for problem files
+PROBLEM_SUFFIX = ".tex"  # Suffix for problem files
+EXAM_TITLE = "Sample Exam"  # Title for main.tex
+SOL_TITLE = "Sample Exam Solutions"  # Title for main_solutions.tex
+AUTHOR = ""  # Author name (leave blank for none)
+DATE = ""  # Date (leave blank for none)
+MARGIN = "1in"  # Margin size for geometry package
+HEADER = False  # If True, include author/date in header
 
 EXAM_TEMPLATE = r"""% main.tex - Auto-generated LaTeX file
 \documentclass[addpoints]{{exam}}
 \usepackage[utf8]{{inputenc}}
 \usepackage{{amssymb}}
 \usepackage{{amsmath}}
-\usepackage[margin=1in]{{geometry}}
-\title{{Sample Exam}}
-\author{{}}
-\date{{}}
+\usepackage[margin={margin}]{{geometry}}
+\title{{{exam_title}}}
+\author{{{author}}}
+\date{{{date}}}
 
 \begin{{document}}
 \maketitle
@@ -30,10 +37,10 @@ SOL_TEMPLATE = r"""% main_solutions.tex - Auto-generated LaTeX file
 \usepackage[utf8]{{inputenc}}
 \usepackage{{amssymb}}
 \usepackage{{amsmath}}
-\usepackage[margin=1in]{{geometry}}
-\title{{Sample Exam Solutions}}
-\author{{}}
-\date{{}}
+\usepackage[margin={margin}]{{geometry}}
+\title{{{sol_title}}}
+\author{{{author}}}
+\date{{{date}}}
 
 \begin{{document}}
 \maketitle
@@ -57,9 +64,23 @@ def main():
         print(f"Please select a number between 1 and {len(files)}.")
         return
     selected = random.sample(files, n)
-    questions = "\n".join([f"    \\input{{{BANK_DIR}/{fname}}}" for fname in selected])
-    tex_content = EXAM_TEMPLATE.format(questions=questions)
-    sol_content = SOL_TEMPLATE.format(questions=questions)
+    # Add \vfill between problems for main.tex (questions only), including after the last
+    questions = "\n\\vfill\n".join([f"    \\input{{{BANK_DIR}/{fname}}}" for fname in selected]) + "\n\\vfill\n"
+    sol_questions = "\n".join([f"    \\input{{{BANK_DIR}/{fname}}}" for fname in selected])
+    tex_content = EXAM_TEMPLATE.format(
+        exam_title=EXAM_TITLE,
+        author=AUTHOR,
+        date=DATE,
+        margin=MARGIN,
+        questions=questions
+    )
+    sol_content = SOL_TEMPLATE.format(
+        sol_title=SOL_TITLE,
+        author=AUTHOR,
+        date=DATE,
+        margin=MARGIN,
+        questions=sol_questions
+    )
     with open("main.tex", "w") as f:
         f.write(tex_content)
     with open("main_solutions.tex", "w") as f:
